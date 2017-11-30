c69be09bbfa0b98b0fdda4134cd141f69d69b7cf
Martin Safar <1martinsafar@gmail.com>
2017-11-30 21:04:02 +0100

    HOME page finished, 1st JS added

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/* ======================================================================
+                        SCRIPTS
+========================================================================= */
+
+// Hide the Notification on click
+const alert = document.querySelector(".alert");
+const alert_close = document.querySelector(".alert-close");
+
+alert_close.addEventListener("click", function() {
+  alert.style.display = "none";
+});
+
+
+
+
+
+
+
+
+//
