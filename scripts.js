3c975d665eedea618f6444002315ce307c002354
Martin Safar <1martinsafar@gmail.com>
2017-12-01 09:56:26 +0100

    Notification improved

--- scripts.js
+++ scripts.js
@@ -3,12 +3,34 @@
 ========================================================================= */
 
 // Hide the Notification on click
-const alert = document.querySelector(".alert");
-const alert_close = document.querySelector(".alert-close");
+const notification = document.querySelector(".notification");
+const notification_close = document.querySelector(".notification-close");
+const close_hover = document.querySelector("#close-hover")
+const notification_text = document.querySelector(".notification p");
 
-alert_close.addEventListener("click", function() {
-  alert.style.display = "none";
+// console.log(notification);
+// console.log(notification_close);
+
+// CLOSING THE NOTIFICATION
+notification_close.addEventListener("click", function() {
+  notification.style.display = "none";
 });
+notification_text.addEventListener("click", function() {
+  notification.style.display = "none";
+})
+// HIGHLIGHTING THE NOTIFICATION CLOSE BUTTON ON HOVER
+notification.addEventListener("mouseover", function() {
+  close_hover.style.color = "white";
+});
+notification.addEventListener("mouseout", function() {
+  close_hover.style.color = "black";
+})
+
+// // HIGHLIGHTING by blinking a few times
+// $( ".notification" ).mouseover(function() {
+//   $("#close-hover").toggle("highlight", {}, 3000);
+// });
+
 
 
 
