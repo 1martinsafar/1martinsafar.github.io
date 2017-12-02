98482df05e000a38b756921c37831720846e8701
Martin Safar <1martinsafar@gmail.com>
2017-12-02 20:12:21 +0100

    JavaScript/jQuery in Browser note added

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@
 // Hide the Notification on click
 const notification = document.querySelector(".notification");
 const notification_close = document.querySelector(".notification-close");
-const close_hover = document.querySelector("#close-hover")
+const close_hover = document.querySelector("#close-hover");
 const notification_text = document.querySelector(".notification p");
 
 // CLOSING THE NOTIFICATION
@@ -14,14 +14,14 @@ notification_close.addEventListener("click", function() {
 });
 notification_text.addEventListener("click", function() {
   notification.style.display = "none";
-})
+});
 // HIGHLIGHTING THE NOTIFICATION CLOSE BUTTON ON HOVER
 notification.addEventListener("mouseover", function() {
   close_hover.style.color = "white";
 });
 notification.addEventListener("mouseout", function() {
   close_hover.style.color = "black";
-})
+});
 
 // // HIGHLIGHTING by blinking a few times
 // $( ".notification" ).mouseover(function() {
